20400c319c8a444b4344c6eccab920aaf13225bc
Leifhaa <leifhaa@hotmail.com>
2022-05-10 01:55:13 +0200

    Fixes for main

--- src/main/java/src/Main.java
+++ src/main/java/src/Main.java
@@ -23,12 +23,46 @@ public class Main {
         else if (m == 1){
             runParallel(n, seed, nThreads);
         }
+        else if (m == 2){
+            runBenchmark(100, seed, nThreads);
+            runBenchmark(1000, seed, nThreads);
+            runBenchmark(10000, seed, nThreads);
+            runBenchmark(100000, seed, nThreads);
+            runBenchmark(1000000, seed, nThreads);
+            runBenchmark(10000000, seed, nThreads);
+        }
 
 
 
         //Oblig5Precode precode = new Oblig5Precode(para.chart, para.coHull);
         //precode.margin = 200;
         //precode.drawGraph();
+    }
+
+    private static void runBenchmark(int n, int seed, int nThreads) {
+        System.out.println("Benchmarking " + n + ". Please wait...");
+        double seqTimes[] = new double[totalRuns];
+        double paraTimes[] = new double[totalRuns];
+        for (int runId = 0; runId < totalRuns; runId++){
+            //Seq
+            ConvexHull chart = new ConvexHull(n);
+            SequentialConvexHull seq = new SequentialConvexHull(chart);
+            seq.populateChart(n, seed);
+            long seqTime = System.nanoTime();
+            seq.find();
+            seqTimes[runId] = (System.nanoTime() - seqTime) / 1000000.0;
+
+            //para
+            ParaConvexHull para = new ParaConvexHull(n, seed, nThreads);
+            long paraTime = System.nanoTime();
+            para.find();
+            paraTimes[runId] = (System.nanoTime() - paraTime) / 1000000.0;
+        }
+        double seqMedian = seqTimes[(seqTimes.length) / 2];
+        double paraMedian = paraTimes[(paraTimes.length) / 2];
+        System.out.println("Median time of sequential run: " + seqMedian + "ms" );
+        System.out.println("Median time of paralell run: " + paraMedian + "ms" );
+        System.out.println("Total speedup: " + seqMedian / paraMedian + " for number " + n);
     }
 
     private static void runSequential(int n, int seed, int nThreads){
